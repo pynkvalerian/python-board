2cd2ecf693066a4d96d8d247af26594dbcf5e2be
pynkvalerian <yenping.chew90@gmail.com>
2019-12-10 13:33:30 +0800

    added forms and reusable templates

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from boards.models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from boards.models import Board, Topic, Post
+from boards.forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -7,4 +9,17 @@ def home(request):
 
 def board_topics(request, pk):
     board = Board.objects.get(pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    form = NewTopicForm()
+    return render(request, 'topics.html', {'board': board, 'form': form})
+
+def new_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save()
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'topics.html', {'form': form, 'board': board})
--- python-board/urls.py
+++ python-board/urls.py
@@ -21,5 +21,6 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('boards/<int:pk>', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/topics', views.new_topics, name="new_topics"),
     path('admin/', admin.site.urls),
 ]
